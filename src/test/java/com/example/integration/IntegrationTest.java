7ffbfeabcd7b6b0cff9171b93ec6a9873b5e7829
micha <michal.aniol.szczesniak@gmail.com>
2025-04-29 21:16:27 +0200

    Additional assertions for test shouldSaveBasic

--- src/test/java/com/example/integration/IntegrationTest.java
+++ src/test/java/com/example/integration/IntegrationTest.java
@@ -3,6 +3,8 @@ package com.example.integration;
 import com.example.dto.BasicDto;
 import com.example.dto.SubBasicDto;
 import com.example.environment.IntegrationTestBase;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
@@ -10,12 +12,16 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.IntStream;
 
 import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.is;
 
 @MicronautTest
 public class IntegrationTest extends IntegrationTestBase {
+
+    ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
     void shouldReturnEmptyResponse(RequestSpecification spec) {
@@ -36,7 +42,7 @@ public class IntegrationTest extends IntegrationTestBase {
     }
 
     @Test
-    void shouldSaveBasic(RequestSpecification spec) {
+    void shouldSaveBasic(RequestSpecification spec) throws JsonProcessingException {
         final var request = new BasicDto(null,
                                             randomAlphabetic(10),
                                             randomAlphabetic(20),
@@ -54,10 +60,22 @@ public class IntegrationTest extends IntegrationTestBase {
                 .extract()
                 .as(BasicDto.class);
 
+        assertThat(savedBasic).isNotNull();
+        assertThat(savedBasic.id()).isNotNull();
+        assertThat(savedBasic.name()).isEqualTo(request.name());
+        assertThat(savedBasic.description()).isEqualTo(request.description());
+        assertThat(savedBasic.subBasics()).hasSize(request.subBasics().size());
+        IntStream.range(0, request.subBasics().size() - 1)
+                         .forEach(i -> {
+                             assertThat(savedBasic.subBasics().get(i).x()).isEqualTo(request.subBasics().get(i).x());
+                             assertThat(savedBasic.subBasics().get(i).y()).isEqualTo(request.subBasics().get(i).y());
+                         });
+
         spec
                 .pathParam("id", savedBasic.id())
                 .get("api/v1/basic/{id}")
                 .then()
-                .statusCode(200);
+                .statusCode(200)
+                .body(is(objectMapper.writeValueAsString(savedBasic)));
     }
 }
